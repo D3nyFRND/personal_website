5f5bea082028e67decb6a984a0ff30dad7fa62fa
Dominik Zalewski <dominikzalewsi@gmail.com>
2023-05-07 12:33:57 +0200

    Copy and pasted files from original vue project

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import About from '../views/AboutView.vue'
+import CV from '../views/CVView.vue'
+import Portfolio from '../views/Portfolio.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'about',
+    component: About
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/cv',
+    name: 'cv',
+    component: CV, 
+  },
+  {
+    path: '/portfolio',
+    name: 'portfolio',
+    component: Portfolio
   }
 ]
 
